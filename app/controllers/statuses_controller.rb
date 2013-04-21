7dbe8503c3c14d7b4652ec2897d2e3f19e68698f
James Robey <james.robey@gmail.com>
2013-04-21 15:58:33 -0700

    MVP achieved, almost-twitter-like input with last status showing

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,2 +1,25 @@
 class StatusesController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @statuses = User.all.map {|x| x.statuses.first}.compact
+  end
+
+  def new
+    @status = Status.new
+  end
+
+  def create
+    @status = current_user.statuses.new(params[:status])
+
+    respond_to do |format|
+      if @status.save
+        format.html { redirect_to(statuses_path, :notice => 'Status was successfully created.') }
+        format.js { render :partial => @status }
+      else
+        format.html { render :action => "new" }
+      end
+    end
+  end
+
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,5 @@
 class Status < ActiveRecord::Base
   attr_accessible :entry
+
+  belongs_to :user
 end
