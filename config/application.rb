3fed6505fc29c5b84f41b980726f852760680efd
James Robey <james.robey@gmail.com>
2013-04-21 07:12:06 -0700

    added devise & devise views

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Boardapp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # devise recommendation for heroku
+    config.assets.initialize_on_precompile = false
   end
 end
