47575cc7eb6b77059dbfb4f757c351a965387e85
James Robey <james.robey@gmail.com>
2013-04-22 07:09:42 -0700

    db seeds for example users added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts 'DEFAULT USERS'
+user2 = User.find_or_create_by_email :name => 'Joe Silver', :email => 'silver@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+user2.statuses.create(entry: "Available")
+user3 = User.find_or_create_by_email :name => 'Sarah Gold', :email => 'gold@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+user3.statuses.create(entry: "In a meeting")
+user4 = User.find_or_create_by_email :name => 'Bob Platinum', :email => 'platinum@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+user4.statuses.create(entry: "Out to lunch")
+puts "users: #{user2.name}, #{user3.name}, #{user4.name}"
